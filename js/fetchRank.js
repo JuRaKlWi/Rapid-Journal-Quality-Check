10ecaab46b276e823b7ea95941ed39a77461d395
Julian R.K. Wichmann <jrk.devgit@gmail.com>
2022-08-22 14:02:11 +0200

    v2.0.4
    Avoiding "SSRN Electronic Journals" as a result

--- js/fetchRank.js
+++ js/fetchRank.js
@@ -10,9 +10,9 @@ function fetchRank(node, title, compl, site, elid) {
         
     var xhr = new XMLHttpRequest();
     /* Public API 
-      api_format = "https://api.crossref.org/works?query.bibliographic=" + encodeURIComponent(title + " " + compl) + "&rows=1&select=DOI,container-title";  */
+      api_format = "https://api.crossref.org/works?query.bibliographic=" + encodeURIComponent(title + " " + compl) + "&rows=2&select=DOI,container-title";  */
     /* Polite API */
-      api_format = "https://api.crossref.org/works?query.bibliographic=" + encodeURIComponent(title + " " + compl) + "&rows=1&select=DOI,container-title&mailto=jrk.devgit@gmail.com";  
+      api_format = "https://api.crossref.org/works?query.bibliographic=" + encodeURIComponent(title + " " + compl) + "&rows=2&select=DOI,container-title&mailto=jrk.devgit@gmail.com";  
         xhr.open("GET", api_format, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
@@ -26,12 +26,27 @@ function fetchRank(node, title, compl, site, elid) {
             } else if (resp["total-results"] == 1 & resp.items[0].hasOwnProperty("container-title")) {
                 url = resp.items[0]["container-title"][0];
                 dblp_url = url;
-                doi = resp.items[0]["DOI"];
-            } else if (resp["total-results"] > 1 & resp.items[0].hasOwnProperty("container-title")) {
+                doi = resp.items[0]["DOI"]; 
+           /* } else if (resp["total-results"] > 1 & resp.items[0].hasOwnProperty("container-title") ) {
                 url = resp.items[0]["container-title"][0];
                 dblp_url = url;
-                doi = resp.items[0]["DOI"];
-            } 
+                doi = resp.items[0]["DOI"]; */
+            }  else if (resp["total-results"] > 1 & resp.items[0].hasOwnProperty("container-title")  & resp.items[1].hasOwnProperty("container-title")) {
+                if( resp.items[0]["container-title"][0] == "SSRN Electronic Journal" ) {
+                    url = resp.items[1]["container-title"][0];
+                    dblp_url = url;
+                    doi = resp.items[1]["DOI"];
+                } else {
+                    url = resp.items[0]["container-title"][0];
+                    dblp_url = url;
+                    doi = resp.items[0]["DOI"];
+                }
+            } else if (resp["total-results"] > 1 & resp.items[0].hasOwnProperty("container-title") & !resp.items[1].hasOwnProperty("container-title")) {
+                url = resp.items[0]["container-title"][0];
+                dblp_url = url;
+                doi = resp.items[0]["DOI"]; 
+            }
+            
             
             doi = doi.replaceAll('\\','');
             
