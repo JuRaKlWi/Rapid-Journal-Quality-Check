6b16feadfa188e3d57f8abfe151745b6211ccbf7
Julian R.K. Wichmann <jrk.devgit@gmail.com>
2022-12-12 09:41:39 +0100

    v.3.0.4
    Minor style improvements and data corrections.

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(({ }) => {
+/*chrome.runtime.onInstalled.addListener(({ }) => {
     chrome.storage.local.set({
     ext_on: true,
     SJR: true,
@@ -8,10 +8,47 @@ chrome.runtime.onInstalled.addListener(({ }) => {
     HCERE: false,
     CORE: true,
     CCF: false,
-    DAEN: true,
-    AJG: true,
-    ABDC: false,
+    DAEN: false,
+    AJG: false,
+    ABDC: true,
     FT50: true,
     turbo: true
   });
+});    */
+
+chrome.runtime.onInstalled.addListener(function(details){
+    
+    chrome.storage.local.set({
+        ext_on: true,
+        SJR: true,
+        VHB: false,
+        FNEGE: false,
+        CoNRS: false,
+        HCERE: false,
+        CORE: true,
+        CCF: false,
+        DAEN: false,
+        AJG: false,
+        ABDC: true,
+        FT50: true,
+        turbo: true
+    });
+     
+    chrome.runtime.openOptionsPage();
+   
 });    
+
+/* 
+const installListener = (details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    //runSettingApp();
+    alert("install");
+  }
+
+  if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    //runSettingApp();
+    alert("update");
+  }
+};
+chrome.runtime.onInstalled.addListener(installListener);
+*/
