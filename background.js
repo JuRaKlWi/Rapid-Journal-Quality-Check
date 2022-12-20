84b199358e063821b8a720bc6e52367887ae8379
Julian R.K. Wichmann <jrk.devgit@gmail.com>
2022-12-20 18:52:23 +0100

    v3.0.4
    minor style changes

--- background.js
+++ background.js
@@ -1,21 +1,3 @@
-/*chrome.runtime.onInstalled.addListener(({ }) => {
-    chrome.storage.local.set({
-    ext_on: true,
-    SJR: true,
-    VHB: false,
-    FNEGE: false,
-    CoNRS: false,
-    HCERE: false,
-    CORE: true,
-    CCF: false,
-    DAEN: false,
-    AJG: false,
-    ABDC: true,
-    FT50: true,
-    turbo: true
-  });
-});    */
-
 chrome.runtime.onInstalled.addListener(function(details){
     
     chrome.storage.local.set({
@@ -36,19 +18,4 @@ chrome.runtime.onInstalled.addListener(function(details){
      
     chrome.runtime.openOptionsPage();
    
-});    
-
-/* 
-const installListener = (details) => {
-  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    //runSettingApp();
-    alert("install");
-  }
-
-  if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
-    //runSettingApp();
-    alert("update");
-  }
-};
-chrome.runtime.onInstalled.addListener(installListener);
-*/
+});
